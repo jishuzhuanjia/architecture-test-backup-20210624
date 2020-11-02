bc14f8d968fe06a3459afd4969a4d9908c2a6dfa
mfcdebugger <mfcdebugger@163.com>
2020-11-02 13:54:42 +0800

    #elastic api测试# spring boot es添加启动类、删除无用文件

---
+++ elastic-search/src/main/java/com/zj/test/elastic_search/EsApplication.java
@@ -0,0 +1,20 @@
+package com.zj.test.elastic_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* @author: zhoujian
+ * @qq: 2025513
+ * @create-time: 2020/11/2 13:33
+ * @description:
+ * @version: 1.0
+ * @finished: false
+ * @finished-time:
+ */
+@SpringBootApplication
+public class EsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class,args);
+    }
+}
