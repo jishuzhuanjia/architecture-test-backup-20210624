33424ecc250a992eb126a28b152ac4cbbafb1b7e
mfcdebugger <mfcdebugger@163.com>
2020-11-05 15:34:58 +0800

    #Thread测试# Daemon守护线程测试

--- java/src/main/java/com/zj/test/java/lang/thread/DaemonThread.java
+++ java/src/main/java/com/zj/test/java/lang/thread/DaemonThread.java
@@ -8,23 +8,26 @@ public class DaemonThread {
 			@Override
 			public void run() {
 				while(true) {
-					//守护进程只输出有限随机次
+					// 守护进程只输出有限随机次
 					// 只要当前JVM实例中尚存在任何一个非守护线程没有结束，守护线程就全部工作；只有当最后一个非守护线程结束时，守护线程随着JVM一同结束工作。
 					// 说明守护进程就像是守护者一样，服务于用户线程，当所有用户线程都结束了，守护线程就没有存在的意义了，就会跟随jvm一起结束。
 					System.out.println("daemon thread running");
-					/*try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}*/
 				}
 			}
-
 		});
 
-		//将线程设置为守护进程或者用户进程(正常线程),当执行的进程全部是守护进程,javaw虚拟机会退出
+		// 必须在start之前调用,在start方法调用之后设置无效。
 		daemonThread.setDaemon(true);
 		daemonThread.start();
+
+
+		// 主线程等待一会，否则可能无法看到守护线程输出的内容
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		// 主线程执行完，因为这里没有其他的非守护线程,所以守护线程也会结束,jvm退出。
 	}
 }
