ff919cf6491093e98ee8c817473de8196c919bc1
mfcdebugger <mfcdebugger@163.com>
2020-11-10 17:02:37 +0800

    #List测试# 新增ArrayList测试文件

---
+++ java/src/main/java/com/zj/test/java/collection/ArrayListTest.java
@@ -0,0 +1,34 @@
+package com.zj.test.java.collection;
+
+import com.zj.test.util.TestHelper;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/* @author: zhoujian
+ * @qq: 2025513
+ * @create-time: 2020/11/10 15:23
+ * @description:
+ * @version: 1.0
+ * @finished: false
+ * @finished-time:
+ */
+public class ArrayListTest {
+
+    /**
+      * author: 2025513
+      *
+      * 1.ArrayList默认capacity为10。
+      *
+      * 测试思路:
+      *
+      * 结果:
+      *
+      * 结论:
+      */
+    @Test
+    public void test1(){
+
+    }
+
+}
