56bb3b0e216158f647879cde48cd5a6256be8717
mfcdebugger <mfcdebugger@163.com>
2020-11-10 14:55:07 +0800

    #java lang# 位运算符测试

--- java/src/main/java/com/zj/test/java/lang/LangTest.java
+++ java/src/main/java/com/zj/test/java/lang/LangTest.java
@@ -24,36 +24,76 @@ public class LangTest {
      *
      * */
     @Test
-    public void test1(){
+    public void test1() {
         Integer integer1 = -128;
         Integer integer2 = -128;
         // true
-        TestHelper.println(integer1==integer2);
+        TestHelper.println(integer1 == integer2);
 
         Integer integer3 = -129;
         Integer integer4 = -129;
         // false
-        TestHelper.println(integer3==integer4);
+        TestHelper.println(integer3 == integer4);
 
         Integer integer5 = 127;
         Integer integer6 = 127;
         // true
-        TestHelper.println(integer5==integer6);
+        TestHelper.println(integer5 == integer6);
 
         Integer integer7 = 128;
         Integer integer8 = 128;
         // false
-        TestHelper.println(integer7==integer8);
+        TestHelper.println(integer7 == integer8);
 
         Integer integer9 = 11;
         Integer integer10 = 11;
         // true
-        TestHelper.println(integer9==integer10);
+        TestHelper.println(integer9 == integer10);
 
         // 创建新的对象, 不从缓存中获取
         Integer integer11 = new Integer(11);
         Integer integer12 = 11;
         // false
-        TestHelper.println(integer11==integer12);
+        TestHelper.println(integer11 == integer12);
+    }
+
+    /**
+     * author: 2025513
+     *
+     * 2.位运算符测试
+     * &、|、~、^的使用
+     *
+     * <ul>
+     *     <li>1.&: 运算的位都为1，则结果为1，否则为0</li>
+     *     <li>2.|:运算的位中只要有1则结果为1,否则为0</li>
+     *     <li>3.~: 单目运算符，位0换成1,1换成0</li>
+     *     <li>4.^: 运算的位值不相同为1,相同为0</li>
+     * </ul>
+     *
+     * 结果:
+     *
+     * 结论:
+     */
+    @Test
+    public void test2() {
+        // 1.&: 运算的位都为1，则结果为1，否则为0
+        // 13&17
+        // 1101&10001
+        // 预计结果应该是:00001,即为1
+        TestHelper.println("13&17", 13 & 17); // 1
+
+        // 2.|:运算的位中只要有1则结果为1,否则为0
+        //结果应为11101,即16+8+4+1=29
+        TestHelper.println("13&17", 13 | 17);// 29
+
+        // 3.~: 0换成1，1换成0
+        // int值经过~运算后的结果规律：
+        // 当int值value>=0，则结果为-(value+1)
+        // 当int值value<0时，则结果为-value-1
+        TestHelper.println("~17", ~17);// -18
+
+        //4.^
+        // 1101 ^ 10001 -> 11100(28)
+        TestHelper.println("13^17", 13 ^ 17); // 28
     }
 }
