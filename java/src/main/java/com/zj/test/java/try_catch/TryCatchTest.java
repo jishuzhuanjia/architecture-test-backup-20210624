53fd3a2cafa05a1c7b167d9345710b25f77b54b5
mfcdebugger <mfcdebugger@163.com>
2020-11-17 10:20:14 +0800

    #更新述#

--- java/src/main/java/com/zj/test/java/try_catch/TryCatchTest.java
+++ java/src/main/java/com/zj/test/java/try_catch/TryCatchTest.java
@@ -15,7 +15,9 @@ import java.net.SocketException;
  */
 public class TryCatchTest {
 
-    // 参数不能为空，否则抛出NullPointerException
+    /**
+     * @exception NullPointerException
+     */
     public static boolean println(String content) {
         if (null == content) {
             throw new NullPointerException("content can not null");
@@ -24,7 +26,7 @@ public class TryCatchTest {
              * 方法一旦执行了throw语句，该方法后的所有代码都不会执行。调用者也会中断执行,
              * 因为代码出现了错误，已经走上了错误的道路，所以必须终止
              *
-             * 如果想要throw后不终止，需要使用try catch语句来正确处理异常，后面的代码才会继续执行。
+             * 如果想要抛出异常后正常运行，需要使用try catch语句来正确处理异常。
              */
             // Unreachable statement
             // TestHelper.println("code after throw");
@@ -37,12 +39,14 @@ public class TryCatchTest {
     }
 
     /**
-     * <p>
      * 2.throws
-     * 方法中抛出Exception，会要求处理异常，此时我们可以将处理异常的工作交给调用者。就是通过throws关键字，
+     * 方法中抛出throw Exception，会要求处理异常，此时我们可以将处理异常的工作交给调用者。就是通过throws关键字，
+     *
      * 如果多种异常是父子关系，只需要写父类即可，但是这不便于调用者对异常的原因进行细分。
-     * <p>
-     * 注意：T extens RuntimeException: 运行时异常不用添加throws关键字，就算添加了，调用者调用时也不会提醒。
+     *
+     * 注意：
+     * T extens RuntimeException: 运行时异常不用添加throws关键字，就算添加了，调用者调用时也不会提醒。
+     * 运行时异常意味着编程出现了错误，不应该处理异常，需要检查代码。
      */
     public static void findFile(String path) throws FileNotFoundException, SocketException {
         if (StringUtils.isBlank(path))
@@ -55,7 +59,8 @@ public class TryCatchTest {
     /**
      * 3.异常的处理
      * 对于我们能够处理的异常，应尽量处理。可以使用其他逻辑来代替抛出异常。
-     * 对于那些我们无法决定处理方式的异常，应该throws交给调用者来决定如何处理。
+     *
+     * 对于那些我们无法决定处理方式的异常，应该使用throws将最终决定权交给调用者。
      * @param args
      */
     public static void main(String[] args) {
