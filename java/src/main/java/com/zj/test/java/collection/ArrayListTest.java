735f50d4c3daf8c5bd6ed5db0e4d365164d9dde4
mfcdebugger <mfcdebugger@163.com>
2020-11-17 15:46:22 +0800

    #更新#

--- java/src/main/java/com/zj/test/java/collection/ArrayListTest.java
+++ java/src/main/java/com/zj/test/java/collection/ArrayListTest.java
@@ -4,6 +4,7 @@ import com.zj.test.util.TestHelper;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /* @author: zhoujian
  * @qq: 2025513
@@ -16,19 +17,17 @@ import java.util.ArrayList;
 public class ArrayListTest {
 
     /**
-      * author: 2025513
-      *
-      * 1.ArrayList默认capacity为10。
-      *
-      * 测试思路:
-      *
-      * 结果:
-      *
-      * 结论:
-      */
+     * author: 2025513
+     *
+     * 1.ArrayList默认容量(capacity)为10。
+     *
+     * 测试思路:
+     *
+     * 结果:
+     *
+     * 结论:
+     */
     @Test
-    public void test1(){
-
+    public void test1() {
     }
-
 }
