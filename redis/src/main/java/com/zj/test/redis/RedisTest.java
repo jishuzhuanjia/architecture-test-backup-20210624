c9e09d0e7af205dd4afc63a7b00dfed9e9bc0f9a
mfcdebugger <mfcdebugger@163.com>
2020-11-20 10:20:11 +0800

    #测试文件创建# RedisTest

---
+++ redis/src/main/java/com/zj/test/redis/RedisTest.java
@@ -0,0 +1,12 @@
+package com.zj.test.redis;
+
+/* @author: zhoujian
+ * @qq: 2025513
+ * @create-time: 2020/11/19 11:13
+ * @description:
+ * @version: 1.0
+ * @finished: false
+ * @finished-time:
+ */
+public class RedisTest {
+}
