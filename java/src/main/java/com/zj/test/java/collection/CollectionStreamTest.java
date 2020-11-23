30e577e1e369bfc513759a941e89e141edb7bcce
mfcdebugger <mfcdebugger@163.com>
2020-11-24 00:39:09 +0800

    #整理#

--- java/src/main/java/com/zj/test/java/collection/CollectionStreamTest.java
+++ java/src/main/java/com/zj/test/java/collection/CollectionStreamTest.java
@@ -19,9 +19,33 @@ import java.util.stream.Collectors;
 public class CollectionStreamTest {
 
     // Collection.stream()测试PO
-    @Setter
-    @Getter
+    /*@Setter
+    @Getter*/
     class ListStreamTestUserPO {
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
         String username;
         String password;
         int age;
