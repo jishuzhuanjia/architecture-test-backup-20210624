e4156a9d5c82a97b6c68b9d98b005e2c3cb26f1f
mfcdebugger <mfcdebugger@163.com>
2020-11-28 19:58:18 +0800

    #api测试# HashMap containsKey和containsValue方法测试

--- java/src/main/java/com/zj/test/java/util/map/HashMapTest.java
+++ java/src/main/java/com/zj/test/java/util/map/HashMapTest.java
@@ -58,7 +58,51 @@ public class HashMapTest {
         /*
         2.设置value为null
          */
-        map.put("key1",null);
+        map.put("key1", null);
         TestHelper.println("map", map);
+    }
+
+    /**
+     * author: jishuzhuanjia
+     * qq: 2025513
+     *
+     * 2.api测试
+     * public boolean containsKey(Object key)
+     * public boolean containsValue(Object value)
+     *
+     * 【作用】
+     *
+     * 【测试结果】
+     *
+     * 【结论】
+     * 1.public boolean containsKey(Object key)
+     * 判断是否存在指定的key,如果存在返回true,否则返回false。
+     *
+     * 2.public boolean containsValue(Object value)
+     * 判断是否存在指定的value，如果存在，返回true，否则返回false
+     *
+     * 【优点】
+     * 【缺点】
+     */
+    @Test
+    public void containsKey_containsValue() {
+
+        HashMap<Object, Object> hashMap = new HashMap<Object, Object>();
+
+        hashMap.put("key1", "value1");
+        hashMap.put("key2", "value2");
+        hashMap.put("key3", "value3");
+
+        TestHelper.println("hashMap",hashMap);
+
+        // 1.public boolean containsKey(Object key)
+        TestHelper.println(" hashMap.containsKey(\"key1\")", hashMap.containsKey("key1"));
+
+        TestHelper.println(" hashMap.containsKey(\"key4\")", hashMap.containsKey("key4"));
+
+        // 2.public boolean containsValue(Object value)
+        TestHelper.println(" hashMap.containsValue(\"value2\")", hashMap.containsValue("value2"));
+        TestHelper.println(" hashMap.containsValue(\"value4\")", hashMap.containsValue("value4"));
+
     }
 }
