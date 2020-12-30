b9e5a931ff2721b06d2788f8a924c0485bb3c446
mfcdebugger <mfcdebugger@163.com>
2020-12-30 17:22:58 +0800

    Revert "#kafka测试# 报错org.springframework.kafka.listener.ListenerExecutionFailedException: invokeHandler Failed; nested exception is java.lang.IllegalStateException: No Acknowledgment available as an argument, the listener container must have a MANUAL AckMode to populate the Acknowledgment.; nested exception is java.lang.IllegalStateException: No Acknowledgment available as an argument, the listener container must have a MANUAL AckMode to populate the Acknowledgment."
    This reverts commit 822682f3

--- rpc-alibaba-customer/src/main/java/com/zj/test/dubbocustomer/service/impl/DubboProviderTestServiceImpl.java
+++ rpc-alibaba-customer/src/main/java/com/zj/test/dubbocustomer/service/impl/DubboProviderTestServiceImpl.java
@@ -73,7 +73,7 @@ public class DubboProviderTestServiceImpl implements DubboCustomerTestService {
      * 越后面的线程等待的时间越久。因此任务还没有执行就已经浪费了很多的启动时间，这种重试方式实现的多线程可用在对速度要求不是很高的场景下，并且它在
      * 一定程度上降低主机的压力。
      * */
-    @Reference(timeout = -1,retries = 1,check = false)
+    @Reference(timeout = -1,retries = 1,lazy = false,check = true)
     DubboProviderTestService dubboProviderTestService;
 
     @Override
