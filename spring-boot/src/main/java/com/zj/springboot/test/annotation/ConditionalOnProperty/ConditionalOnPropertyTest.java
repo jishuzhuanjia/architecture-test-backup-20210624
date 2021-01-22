ad4f076821b1cfcc43b58453855fbb0ff399b2be
mfcdebugger <mfcdebugger@163.com>
2021-01-22 15:31:11 +0800

    #测试# spring boot测试

--- spring-boot/src/main/java/com/zj/springboot/test/annotation/ConditionalOnProperty/ConditionalOnPropertyTest.java
+++ spring-boot/src/main/java/com/zj/springboot/test/annotation/ConditionalOnProperty/ConditionalOnPropertyTest.java
@@ -3,6 +3,7 @@ package com.zj.springboot.test.annotation.ConditionalOnProperty;
 import com.zj.test.util.TestHelper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 /* @author: zhoujian
  * @qq: 2025513
@@ -12,10 +13,10 @@ import org.springframework.context.annotation.Configuration;
  * @finished: false
  * @finished-time:
  */
-@Configuration
+@Component
 // name和value属性是互斥的,不能同时出现,否则报错:
 // The name and value attributes of @ConditionalOnProperty are exclusive,如果想要匹配值,使用havingValue
-@ConditionalOnProperty(prefix = "xxx",name = "username",havingValue = "usernameInConditional1",/*value = "usernameInConditional",*/
+@ConditionalOnProperty(prefix = "xxx",name = "username",havingValue = "usernameInConditional",/*value = "usernameInConditional",*/
         matchIfMissing = false)
 public class ConditionalOnPropertyTest {
 
