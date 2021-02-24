413023c28cc3e3811feb00644b7337a891458b3a
jishuzhuanjia <mfcdebugger@163.com>
2021-02-24 23:15:10 +0800

    #更新#

--- elastic-search/src/main/java/com/zj/test/elastic_search/util/ElasticsearchUtil.java
+++ elastic-search/src/main/java/com/zj/test/elastic_search/util/ElasticsearchUtil.java
@@ -55,11 +55,11 @@ public class ElasticsearchUtil {
     /**
      * 如果索引不存在，则创建索引
      *
-     * @finished-Time: 2021年2月22日 09:52:59
-     * @param index 索引名
-     * @return 索引已经存在或创建失败, 返回false, 否则返回true。
+     * @finished-Time                           2021年2月22日 09:52:59
+     * @param  index                            索引名
+     * @return                                  索引已经存在或创建失败, 返回false, 否则返回true。
      *
-     * @exception InvalidIndexNameException 运行时异常,索引名无效，如索引名字母必须全部小写,否则会抛出此异常
+     * @exception  InvalidIndexNameException    运行时异常,索引名无效，如索引名字母必须全部小写,否则会抛出此异常
      *
      */
     public static boolean createIndex(String index) {
@@ -77,9 +77,9 @@ public class ElasticsearchUtil {
     /**
      * 删除索引
      *
-     * @finished-time: 2021年2月22日 10:41:15
-     * @param index 要删除的索引名
-     * @return 如果成功删除返回true, 索引不存在或删除失败则返回false。
+     * @finished-time   2021年2月22日 10:41:15
+     * @param  index    要删除的索引名
+     * @return          如果成功删除返回true, 索引不存在或删除失败则返回false。
      */
     public static boolean deleteIndex(String index) {
         if (!isIndexExist(index)) {
@@ -99,8 +99,8 @@ public class ElasticsearchUtil {
      * 判断索引是否存在
      * 说明：由于判断索引是否存在所需时间较删除/创建索引微不足道(1:10左右)，因此在删除/创建索引之前进行索引存在判断可减少用时。
      *
-     * @finish-time: 2021年2月22日 16:38:11
-     * @param index
+     * @finish-time  2021年2月22日 16:38:11
+     * @param index  索引
      * @return
      */
     public static boolean isIndexExist(String index) {
@@ -112,7 +112,7 @@ public class ElasticsearchUtil {
     /**
      * 判断index下指定type是否存在
      *
-     * @finished-time: 2021年2月22日 17:18:13
+     * @finished-time  2021年2月22日 17:18:13
      */
     public boolean isTypeExist(String index, String type) {
         return isIndexExist(index)
@@ -123,15 +123,15 @@ public class ElasticsearchUtil {
     /**
      * 插入/更新：根据指定的id插入/更新数据，这取决于指定id的数据是否已经存在，如果存在，则是更新操作。
      *
-     * @tip 为了与update操作区分开，请使用update方法进行更新操作。
-     * @tip 不要进行批量操作，性能不好
-     * @tip index type参数不能省略，如果索引没有完成映射，会自动完成索引和type的映射，如果已经完成映射，则index 和 type必须与已有对应，否则报错
-     * @finished-time: 2021年2月22日 17:18:22
-     * @param jsonObject 要插入的数据
-     * @param index      索引，类似数据库
-     * @param type       类型，类似表
-     * @param id         数据id
-     * @return 返回插入/更新的数据的id
+     * @tip                 为了与update操作区分开，请使用update方法进行更新操作。
+     * @tip                 不要进行批量操作，性能不好
+     * @tip                 index type参数不能省略，如果索引没有完成映射，会自动完成索引和type的映射，如果已经完成映射，则index 和 type必须与已有对应，否则报错
+     * @finished-time       2021年2月22日 17:18:22
+     * @param  jsonObject   要插入的数据
+     * @param  index        索引，类似数据库
+     * @param  type         类型，类似表
+     * @param  id           数据id
+     * @return              返回插入/更新的数据的id
      */
     public static String insertDataUseSpecifiedId(JSONObject jsonObject, String index, String type, String id) {
         // get() = execute().actionGet()
@@ -148,11 +148,11 @@ public class ElasticsearchUtil {
     /**
      * 插入数据
      *
-     * @finished-time: 2021年2月23日 09:46:26
-     * @tip 不要进行批量操作，性能不好
-     * @param jsonObject 要增加的数据
-     * @param index      索引，类似数据库
-     * @param type       类型，类似表
+     * @finished-time       2021年2月23日 09:46:26
+     * @tip                 不要进行批量操作，性能不好
+     * @param  jsonObject   要增加的数据
+     * @param  index        索引，类似数据库
+     * @param  type         类型，类似表
      * @return
      */
     public static String insertDataUseRandomId(JSONObject jsonObject, String index, String type) {
@@ -163,10 +163,10 @@ public class ElasticsearchUtil {
     /**
      * 通过id删除数据
      *
-     * @finish-time 2021年2月23日 09:57:38
-     * @param index 索引，类似数据库
-     * @param type  类型，类似表
-     * @param id    数据ID
+     * @finish-time     2021年2月23日 09:57:38
+     * @param  index    索引，类似数据库
+     * @param  type     类型，类似表
+     * @param  id       数据ID
      */
     public static void deleteDataById(String index, String type, String id) {
         DeleteResponse response = client.prepareDelete(index, type, id).execute().actionGet();
@@ -176,12 +176,13 @@ public class ElasticsearchUtil {
     /**
      * 通过id更新数据
      *
-     * @finish-time 2021年2月23日 10:01:21
-     * @tip 如果指定的id数据不存在，就什么都不做
-     * @param jsonObject 要增加的数据
-     * @param index      索引，类似数据库
-     * @param type       类型，类似表
-     * @param id         数据ID
+     * @finish-time             2021年2月23日 10:01:21
+     * @tip                     如果指定的id数据不存在，就什么都不做
+     *
+     * @param  jsonObject       要增加的数据
+     * @param  index            索引，类似数据库
+     * @param  type             类型，类似表
+     * @param  id               数据id
      * @return
      */
     public static void updateDataById(JSONObject jsonObject, String index, String type, String id) {
@@ -193,11 +194,11 @@ public class ElasticsearchUtil {
     /**
      * 通过id获取数据
      *
-     * @finish-time 2021年2月23日 10:16:26
-     * @param index  索引，类似数据库
-     * @param type   类型，类似表
-     * @param id     数据id
-     * @param fields 需要显示的字段，逗号分隔（缺省为全部字段）
+     * @finish-time     2021年2月23日 10:16:26
+     * @param  index    索引，类似数据库
+     * @param  type     类型，类似表
+     * @param  id       数据id
+     * @param  fields   需要显示的字段，逗号分隔（缺省为全部字段）
      * @return
      */
     public static Map<String, Object> searchDataById(String index, String type, String id, String fields) {
@@ -212,27 +213,33 @@ public class ElasticsearchUtil {
     /**
      * 结构化查询，将查询数据封装到到分页中
      *
-     * @attention 高亮字段结果将会覆盖正常返回的字段的结果
-     * @attention 高亮字段需要作为查询的条件，否则查询结果不会返回highlight而报空指针异常
-     * @attention type传参: 对于低/高版本es,会查询索引中的多个索引，如果都不存在，不会报错，查询结果为空。
-     *                       如果不传参，则会查询索引所有存在的type
-     *                       兼容高低版本的es
-     * @finish-time: 2021年2月23日 14:18:54
+     * @attention                       高亮字段结果将会覆盖正常返回的字段的结果
+     * @attention                       高亮字段需要作为查询的条件，否则查询结果不会返回highlight而报空指针异常
+     * @attention                       type传参: 对于低/高版本es,会查询索引中的多个索引，如果都不存在，不会报错，查询结果为空。
+     *                                  如果不传参，则会查询索引所有存在的type
+     *                                  兼容高低版本的es
+     * @finish-time                     2021年2月23日 14:18:54
      *
      *
-     * @param index          索引名称
-     * @param type           索引type,低版本中索引支持多个type,高版本中最多只有一个type,但是不管传递1或多个type都不会报错。
-     * @param currentPage           开始行,基于0
-     * @param pageSize           每页显示条数
-     * @param query          查询条件
-     * @param fields         需要显示的字段，逗号分隔（缺省为全部字段）
-     * @param sortField      排序字段
-     * @param highlightFieldsStr 高亮字段
+     * @param  index                        索引名称
+     * @param  type                         索引type,低版本中索引支持多个type,高版本中最多只有一个type,
+     *                                      但是不管传递1或多个type都不会报错。
+     * @param  currentPage                  开始行,基于0
+     * @param  pageSize                     每页显示条数
+     * @param  query                        查询条件
+     * @param  fieldsStr                    需要显示的字段，逗号分隔（缺省为全部字段）
+     *                                      TIP: 字段必须以,隔开,且不能有多余的空格。
+     * @param  sortField                    排序字段
+     * @param  highlightFieldsStr           高亮字段
+     *                                      TIP: 字段必须以,隔开,且不能有多余的空格。
      * @return
      */
-    public static EsPage searchDataAsPage(String index, String type, int currentPage, int pageSize, QueryBuilder query, String fields, String sortField, String highlightFieldsStr) {
-        String[] highlightFields = getHighlightArray(highlightFieldsStr);
-        SearchResponse searchResponse = sendSearchRequest(index, type, currentPage, pageSize, query, fields, sortField, highlightFields);
+    public static EsPage searchDataAsPage(String index, String type,
+                                          int currentPage, int pageSize, QueryBuilder query,
+                                          String fieldsStr, String sortField, String highlightFieldsStr) {
+        //String[] highlightFields = getHighlightArray(highlightFieldsStr);
+        String[] highlightFields = (highlightFieldsStr == null) ? null : highlightFieldsStr.split(",");
+        SearchResponse searchResponse = sendSearchRequest(index, type, currentPage, pageSize, query, fieldsStr, sortField, highlightFields);
 
         long totalHits = searchResponse.getHits().totalHits;
         long length = searchResponse.getHits().getHits().length;
@@ -248,35 +255,37 @@ public class ElasticsearchUtil {
         return null;
     }
 
-
-
-
     /**
      * 结构化查询，将查询数据封装到到列表中
      *
-     * @attention 高亮字段结果将会覆盖正常返回的字段的结果
-     * @attention 高亮字段需要作为查询的条件，否则查询结果不会返回highlight而报空指针异常
-     * @attention type传参: 对于低/高版本es,会查询索引中的多个索引，如果都不存在，不会报错，查询结果为空。
-     *                       如果不传参，则会查询索引所有存在的type
-     *                       兼容高低版本的es
+     * @attention                   高亮字段结果将会覆盖正常返回的字段的结果
+     * @attention                   高亮字段需要作为查询的条件，否则查询结果不会返回highlight而报空指针异常
+     * @attention                   type传参: 对于低/高版本es,会查询索引中的多个索引，如果都不存在，不会报错，查询结果为空。
+     *                              如果不传参，则会查询索引所有存在的type
+     *                              兼容高低版本的es
      *
-     * @param index          索引名称
-     * @param type           索引type,低版本中索引支持多个type,高版本中最多只有一个type,但是不管传递1或多个type都不会报错。
-     * @param currentPage           开始行,基于0
-     * @param pageSize           每页显示条数
-     * @param query          查询条件
-     * @param fields         需要显示的字段，逗号分隔（缺省为全部字段）
-     * @param sortField      排序字段
-     * @param highlightFieldsStr 高亮字段
+     * @param  index                索引名称
+     * @param  type                 索引type,低版本中索引支持多个type,高版本中最多只有一个type,但是不管传递1或多个type都不会报错。
+     * @param  currentPage          开始行,基于0
+     * @param  pageSize             每页显示条数
+     * @param  query                查询条件
+     * @param  fieldsStr            需要显示的字段，逗号分隔（缺省为全部字段）
+     *                              TIP: 字段必须以,隔开,且不能有多余的空格。
+     * @param  sortField            排序字段
+     * @param  highlightFieldsStr   高亮字段
+     *                              TIP: 字段必须以,隔开,且不能有多余的空格。
      * @return
      */
-    public static List<Map<String, Object>> searchDataAsList(String index, String type, int currentPage, int pageSize, QueryBuilder query, String fields, String sortField, String highlightFieldsStr) {
-        String[] highlightArray = getHighlightArray(highlightFieldsStr);
-        SearchResponse searchResponse = sendSearchRequest(index, type, currentPage, pageSize, query, fields, sortField, highlightArray);
+    public static List<Map<String, Object>> searchDataAsList(String index, String type,
+                                                             int currentPage, int pageSize, QueryBuilder query,
+                                                             String fieldsStr, String sortField, String highlightFieldsStr) {
+        //String[] highlightArray = getHighlightArray(highlightFieldsStr);
+        String[] highlightFields = (highlightFieldsStr == null) ? null : highlightFieldsStr.split(",");
+        SearchResponse searchResponse = sendSearchRequest(index, type, currentPage, pageSize, query, fieldsStr, sortField, highlightFields);
 
         if (searchResponse.status().getStatus() == 200) {
             // 解析返回结果
-            return processSearchResponse(searchResponse, highlightArray);
+            return processSearchResponse(searchResponse, highlightFields);
         }
         return null;
     }
@@ -377,7 +386,7 @@ public class ElasticsearchUtil {
         String[] highlightFields = null;
         if (StringUtils.isNotBlank(highlightFieldsStr)) {
             // 字段不能包含空格，所以需要移除空格
-            highlightFields = StringUtils.remove(highlightFieldsStr," ").split(",");
+            highlightFields = StringUtils.remove(highlightFieldsStr, " ").split(",");
         }
         return highlightFields;
     }
@@ -385,7 +394,7 @@ public class ElasticsearchUtil {
     public static void main(String[] args) {
         // 移除指定的字符,不会修改源字符串
         String s = " This is my world!";
-        TestHelper.println(StringUtils.remove(s,' '));
+        TestHelper.println(StringUtils.remove(s, ' '));
         TestHelper.println(s);
     }
 }
