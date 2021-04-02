4aed63a006dfdb4079716e6b93b3b12e79925b7c
jishuzhuanjia <mfcdebugger@163.com>
2021-04-02 15:38:21 +0800

    #更新#

---
+++ java/src/main/java/com/zj/test/java/time/LocalDateTimeTest.java
@@ -0,0 +1,34 @@
+package com.zj.test.java.time;
+
+import com.zj.test.util.TestHelper;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+/* @author: zhoujian
+ * @qq: 2025513
+ * @create-time: 2021/4/2 15:08
+ * @description: LocalDateTime测试
+ * @version: 1.0
+ * @finished: false
+ * @finished-time:
+ */
+public class LocalDateTimeTest {
+
+    /**
+     * 1.LocalDateTime的创建
+     * 【描述】
+     *
+     *
+     * 【返回值】
+     *
+     *
+     * 【注意事项】
+     *
+     *
+     */
+    @Test
+    public void test(){
+        LocalDateTime localDateTime= LocalDateTime.now();
+    }
+}
