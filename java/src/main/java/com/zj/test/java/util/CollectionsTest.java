634d0ce03967422cabb84b0ff3692d9441d572c5
jishuzhuanjia <mfcdebugger@163.com>
2021-06-22 17:10:00 +0800

    #整理#

--- java/src/main/java/com/zj/test/java/util/CollectionsTest.java
+++ java/src/main/java/com/zj/test/java/util/CollectionsTest.java
@@ -3,10 +3,10 @@ package com.zj.test.java.util;
 import com.zj.test.util.TestHelper;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author: zhoujian
@@ -61,5 +61,50 @@ public class CollectionsTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }*/
+    }
+
+    /**
+     * 2.java.util.Set<T> singleton(T o)
+     * 返回只包含指定对象的不可变集合。返回的集合是可序列化的。
+     */
+    @Test
+    public void singleton(){
+        Set<String> set1 = Collections.singleton("我");
+        // 不能添加元素,否则: java.lang.UnsupportedOperationException
+        /*set1.add("我");
+        set1.add("爱");
+        set1.add("你");*/
+
+        TestHelper.println(set1);
+    }
+
+    /**
+     * 3.List<T> singletonList(T o)
+     * 于singleton不同的是，返回的是一个List，也不能够修改元素
+     *
+     */
+    @Test
+    public void singletonList(){
+        List<String> list = Collections.singletonList("我");
+
+        // java.lang.UnsupportedOperationException
+        /*list.add("爱");
+        list.add("你");*/
+
+        TestHelper.println(list);
+
+    }
+
+    /**
+     * 4.java.util.Map<K, V> singletonMap(K key,V value)
+     * 与singleton不同的是，返回的是一个Map，也不能够修改元素
+     */
+    @Test
+    public void singletonMap(){
+        Map<String, String> map = Collections.singletonMap("username", "password");
+
+        // 不能够修改map.否则: java.lang.UnsupportedOperationException
+        //map.put("password","123456");
+        TestHelper.println(map);
     }
 }
