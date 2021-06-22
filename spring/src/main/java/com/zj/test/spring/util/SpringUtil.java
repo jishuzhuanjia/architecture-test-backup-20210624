9512458266d9a293576944c5a3fc1d909d6f3c88
jishuzhuanjia <mfcdebugger@163.com>
2021-06-22 15:37:57 +0800

    #更新# SpringUtil

---
+++ spring/src/main/java/com/zj/test/spring/util/SpringUtil.java
@@ -0,0 +1,48 @@
+package com.zj.test.spring.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ *      spring工具
+ * </p>
+ *
+ * @author: zhoujian
+ * @e-mail: 2025513@qq.com
+ * @create-time: 2021/6/22 15:18
+ * @is-finished: false
+ *
+ */
+@Component
+@Slf4j
+public class SpringUtil implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        if (SpringUtil.applicationContext == null) {
+            SpringUtil.applicationContext = applicationContext;
+        }
+        log.info("ApplicationContext配置成功,applicationContext对象：" + SpringUtil.applicationContext);
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
